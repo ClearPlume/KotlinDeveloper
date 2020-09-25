ef9e236252819a31984357f33076e95a5888765b
FallenAngel <1544931543qq.com>
2020-09-25 21:46:48 +0800

    第三项目 MineSweeper ，第二阶段 Flexible mines 完成。

--- Minesweeper/Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/Minesweeper/task/src/minesweeper/Main.kt
@@ -1,17 +1,24 @@
 package minesweeper
 
+import java.util.*
+import kotlin.random.Random
+
 fun main() {
-    val field = arrayOf(
-            charArrayOf('.', '.', '.', '.', '.', '.', '.', 'X', '.'),
-            charArrayOf('.', 'X', '.', '.', '.', '.', '.', '.', '.'),
-            charArrayOf('.', '.', '.', '.', 'X', '.', '.', '.', '.'),
-            charArrayOf('.', '.', '.', '.', '.', '.', '.', '.', 'X'),
-            charArrayOf('.', '.', '.', 'X', '.', '.', 'X', '.', '.'),
-            charArrayOf('.', '.', '.', '.', '.', '.', '.', '.', '.'),
-            charArrayOf('.', '.', '.', 'X', '.', '.', '.', '.', '.'),
-            charArrayOf('.', '.', 'X', '.', '.', 'X', '.', '.', '.'),
-            charArrayOf('.', '.', '.', '.', '.', '.', '.', 'X', '.')
-    )
+    print("How many mines do you want on the field?")
+    val numOfMines = Scanner(System.`in`).nextInt()
+    var curMineNum = 0
+    val field = Array(9) { CharArray(9) { '.' } }
+
+    while (curMineNum < numOfMines) {
+        val mineRow = Random.nextInt(9)
+        val mineCol = Random.nextInt(9)
+
+        if (field[mineRow][mineCol] != 'X') {
+            field[mineRow][mineCol] = 'X'
+            curMineNum++
+        }
+    }
+
     for (f in field) {
         println(f)
     }
