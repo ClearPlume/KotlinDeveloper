591de1df1cb9b313eb44b756ff94ae47345bd9ef
杜海蛟 <the.fallenangel.965@gmail.com>
2021-03-31 19:49:58 +0800

    NumberBaseConverter项目，第二阶段 Convert to decimal in Number Base Converter 完成

--- NumberBaseConverter/Number Base Converter/task/src/converter/Main.kt
+++ NumberBaseConverter/Number Base Converter/task/src/converter/Main.kt
@@ -1,23 +1,58 @@
 package converter
 
 import java.util.*
+import kotlin.math.pow
 
-val HEX_MAPPING = mapOf(10 to "A", 11 to "B", 12 to "C", 13 to "D", 14 to "E", 15 to "F")
+val DEC_HEX_MAPPING = mapOf(10 to "A", 11 to "B", 12 to "C", 13 to "D", 14 to "E", 15 to "F")
+val HEX_DEC_MAPPING = mapOf('a' to 10, 'b' to 11, 'c' to 12, 'd' to 13, 'e' to 14, 'f' to 15)
 
 fun main() {
-    print("Enter number in decimal system:")
-    var original = Scanner(System.`in`).nextInt()
-
-    print("Enter target base:")
-    val base = Scanner(System.`in`).nextInt()
-
-    var result = ""
+    val scanner = Scanner(System.`in`)
 
     do {
-        val remainder = original % base
-        result = (if (remainder > 9) HEX_MAPPING[remainder] else remainder).toString() + result
-        original /= base
-    } while (original > 0)
+        print("Do you want to convert /from decimal or /to decimal? (To quit type /exit)")
+        val command = scanner.next()
 
-    print("Conversion result: $result")
+        when (command) {
+            "/from" -> {
+                print("Enter number in decimal system:")
+                var decimal = Scanner(System.`in`).nextInt()
+
+                print("Enter target base:")
+                val tarBase = Scanner(System.`in`).nextInt()
+
+                var result = ""
+
+                do {
+                    val remainder = decimal % tarBase
+                    result = (if (remainder > 9) DEC_HEX_MAPPING[remainder] else remainder).toString() + result
+                    decimal /= tarBase
+                } while (decimal > 0)
+
+                println("Conversion to decimal result: $result")
+            }
+            "/to" -> {
+                print("Enter source number:")
+                val source = scanner.next()
+
+                print("Enter source base:")
+                val srcBase = scanner.nextInt()
+
+                var result = 0
+                var curDigitNum = source.lastIndex
+
+                for (digit in source) {
+                    result += if (digit.isDigit()) {
+                        srcBase.toDouble().pow(curDigitNum--).toInt() * (digit.toInt() - 48)
+                    } else {
+                        srcBase.toDouble().pow(curDigitNum--).toInt() * HEX_DEC_MAPPING[digit]!!
+                    }
+                }
+
+                println("Conversion to decimal result: $result")
+            }
+            else -> continue
+        }
+
+    } while (command != "/exit")
 }
