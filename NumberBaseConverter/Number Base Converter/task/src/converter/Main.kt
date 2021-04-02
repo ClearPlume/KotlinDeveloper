0429efdebe38fa76b8c5a79da3eeac5ccf7e923f
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-02 16:19:58 +0800

    NumberBaseConverter项目，第三阶段 Convert to any base 完成

--- NumberBaseConverter/Number Base Converter/task/src/converter/Main.kt
+++ NumberBaseConverter/Number Base Converter/task/src/converter/Main.kt
@@ -1,58 +1,59 @@
 package converter
 
+import java.math.BigInteger
 import java.util.*
-import kotlin.math.pow
-
-val DEC_HEX_MAPPING = mapOf(10 to "A", 11 to "B", 12 to "C", 13 to "D", 14 to "E", 15 to "F")
-val HEX_DEC_MAPPING = mapOf('a' to 10, 'b' to 11, 'c' to 12, 'd' to 13, 'e' to 14, 'f' to 15)
+import kotlin.system.exitProcess
 
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    do {
-        print("Do you want to convert /from decimal or /to decimal? (To quit type /exit)")
-        val command = scanner.next()
+    @Suppress("SpellCheckingInspection")
+    val digitChars = "0123456789abcdefghijklmnopqrstuvwxyz"
 
-        when (command) {
-            "/from" -> {
-                print("Enter number in decimal system:")
-                var decimal = Scanner(System.`in`).nextInt()
+    while (true) {
+        print("Enter two numbers in format: {source base} {target base} (To quit type /exit)")
+        val command = readLine()!!
 
-                print("Enter target base:")
-                val tarBase = Scanner(System.`in`).nextInt()
-
-                var result = ""
-
-                do {
-                    val remainder = decimal % tarBase
-                    result = (if (remainder > 9) DEC_HEX_MAPPING[remainder] else remainder).toString() + result
-                    decimal /= tarBase
-                } while (decimal > 0)
-
-                println("Conversion to decimal result: $result")
-            }
-            "/to" -> {
-                print("Enter source number:")
-                val source = scanner.next()
-
-                print("Enter source base:")
-                val srcBase = scanner.nextInt()
-
-                var result = 0
-                var curDigitNum = source.lastIndex
-
-                for (digit in source) {
-                    result += if (digit.isDigit()) {
-                        srcBase.toDouble().pow(curDigitNum--).toInt() * (digit.toInt() - 48)
-                    } else {
-                        srcBase.toDouble().pow(curDigitNum--).toInt() * HEX_DEC_MAPPING[digit]!!
-                    }
-                }
-
-                println("Conversion to decimal result: $result")
-            }
-            else -> continue
+        if (command == "/exit") {
+            exitProcess(0)
         }
 
-    } while (command != "/exit")
+        val (srcBase, tarBase) = command.split(' ').map { BigInteger(it) }
+
+        do {
+            print("Enter number in base $srcBase to convert to base $tarBase (To go back type /back)")
+            val source = (scanner.next())
+
+            if (source == "/back") {
+                break
+            }
+
+            if (srcBase == tarBase) {
+                println("Conversion result: $source")
+                continue
+            }
+
+            var decimal = BigInteger.ZERO
+            var curDigitNum = source.lastIndex
+
+            for (digit in source) {
+                decimal += srcBase.pow(curDigitNum--) * digitChars.indexOf(digit).toBigInteger()
+            }
+
+            if (tarBase.toInt() == 10) {
+                println("Conversion result: $decimal")
+                continue
+            }
+
+            var result = ""
+
+            do {
+                val remainder = decimal % tarBase
+                result += digitChars[remainder.toInt()]
+                decimal /= tarBase
+            } while (decimal > BigInteger.ZERO)
+
+            println("Conversion result: ${result.reversed()}")
+        } while (true)
+    }
 }
