ddfe15758147e15b3fd49fd9a892a0fc3ba66622
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-05 17:04:14 +0800

    Cinema Room Manager 项目，第三阶段 Tickets 完成  每日一题完成

--- CinemaRoomManager/Cinema Room Manager/task/src/cinema/Cinema.kt
+++ CinemaRoomManager/Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,23 +1,32 @@
 package cinema
 
+import java.util.*
+
 fun main() {
     // write your code here
-    val colNum = 9
-    val seats = Array(8) { col ->
+    val scanner = Scanner(System.`in`)
+
+    println("Enter the number of rows:")
+    val rows = scanner.nextInt() + 1
+
+    println("Enter the number of seats in each row:")
+    val cols = scanner.nextInt() + 1
+
+    val seats = Array(rows) { col ->
         if (col == 0) {
-            Array(colNum) { row ->
+            Array(cols) { row ->
                 if (row == 0) {
                     "  "
                 } else {
-                    "$row${if (row < colNum - 1) ' ' else '\u0000'}"
+                    "$row${if (row < cols) ' ' else '\u0000'}"
                 }
             }
         } else {
-            Array(colNum) { row ->
+            Array(cols) { row ->
                 if (row == 0) {
                     "$col "
                 } else {
-                    "S${if (row < colNum - 1) ' ' else '\u0000'}"
+                    "S${if (row < cols) ' ' else '\u0000'}"
                 }
             }
         }
@@ -28,4 +37,41 @@ fun main() {
         it.forEach { seat -> print(seat) }
         println()
     }
+
+    println("Enter a row number:")
+    val row = scanner.nextInt()
+
+    println("Enter a seat number in that row:")
+    val col = scanner.nextInt()
+
+    seats[row][col] = "B "
+
+    val seatsNum = (rows - 1) * (cols - 1)
+
+    val ticketPrice = if (seatsNum <= 60) {
+        10
+    } else {
+        if (row in 1..(rows - 1) / 2) 10 else 8
+    }
+
+    println("Ticket price: $$ticketPrice")
+
+    println("Cinema:")
+    seats.forEach {
+        it.forEach { seat -> print(seat) }
+        println()
+    }
+
+    // val totalIncome = if (seatsNum <= 20) {
+    //     seatsNum * 10
+    // } else {
+    //     if (rows % 2 == 0) {
+    //         cols * (rows / 2) * 10 + cols * (rows / 2) * 8
+    //     } else {
+    //         cols * (rows / 2) * 10 + cols * (rows / 2 + 1) * 8
+    //     }
+    // }
+    //
+    // println("Total income:")
+    // println("$$totalIncome")
 }
