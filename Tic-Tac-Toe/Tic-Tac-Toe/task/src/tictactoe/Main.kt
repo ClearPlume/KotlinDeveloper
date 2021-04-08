c6d95ec85c93a40c3be89ec1c2603c11d337db90
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-08 13:50:44 +0800

    第二阶段 The user is the gamemaster 完成

--- Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -2,7 +2,24 @@ package tictactoe
 
 fun main() {
     // write your code here
-    println("X O X")
-    println("O X O")
-    println("O X O")
+    val gridSize = 3
+
+    print("Enter cells: ")
+    val cells = readLine()?.chunked(gridSize)?.map {
+        var row = ""
+
+        for (i in it.indices.reversed()) {
+            row = if (i > 0) {
+                " ${it.substring(i, i + 1)}" + row
+            } else {
+                it.substring(i, i + 1) + row
+            }
+        }
+
+        row
+    }
+
+    println("---------")
+    cells?.forEach { println("| $it |") }
+    println("---------")
 }
