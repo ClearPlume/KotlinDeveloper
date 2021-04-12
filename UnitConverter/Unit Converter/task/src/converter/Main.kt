3fc8b588004fef8ff94de2534edc362a2a8404f5
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-12 18:26:37 +0800

    第二阶段 Let’s get metric 完成

--- UnitConverter/Unit Converter/task/src/converter/Main.kt
+++ UnitConverter/Unit Converter/task/src/converter/Main.kt
@@ -1,9 +1,8 @@
 package converter
 
 fun main() {
-    println("145 centimeters is 1.45 meters")
-    println("2 miles is 3.2187 kilometers")
-    println("5.5 inches is 139.7 millimeters")
-    println("12 degrees Celsius is 53.6 degrees Fahrenheit")
-    println("3 pounds is 1.360776 kilograms")
+    print("Enter a number of kilometers: ")
+    val kilometers = readLine()!!.toInt()
+
+    println("$kilometers kilometers is ${kilometers * 1000} meters")
 }
---
+++ UnitConverter/Unit Converter/task/test/Authors.kt
@@ -0,0 +1,14 @@
+import java.io.*
+import java.util.*
+
+
+object Authors {
+    fun solve(sin: Scanner, sout: PrintStream) {
+
+        sout.print("Enter a number of kilometers: ")
+
+        val x = sin.nextInt()
+
+        sout.println("$x kilometers is ${x * 1000} meters")
+    }
+}
