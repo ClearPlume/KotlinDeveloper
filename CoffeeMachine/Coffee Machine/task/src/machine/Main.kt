dc2ee1affc92ccf74693a804555f18ca86584a2d
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-14 14:05:52 +0800

    第二阶段 Ingredient calculator 完成

--- CoffeeMachine/Coffee Machine/task/src/machine/Main.kt
+++ CoffeeMachine/Coffee Machine/task/src/machine/Main.kt
@@ -1,15 +1,17 @@
 package machine
 
+import java.util.*
+
 fun main() {
+    print("Write how many cups of coffee you will need:")
+    val cup = Scanner(System.`in`).nextInt()
+
     println(
         """
-        Starting to make a coffee
-        Grinding coffee beans
-        Boiling water
-        Mixing boiled water with crushed coffee beans
-        Pouring coffee into the cup
-        Pouring some milk into the cup
-        Coffee is ready!
+        For $cup cups of coffee you will need:
+        ${cup * 200} ml of water
+        ${cup * 50} ml of milk
+        ${cup * 15} g of coffee beans
     """.trimIndent()
     )
 }
