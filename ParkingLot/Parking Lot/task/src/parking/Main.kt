28dea87c52ee28307859da4e67e052f609ba778b
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-14 19:09:40 +0800

    第二阶段 Only 2 spots 完成

--- ParkingLot/Parking Lot/task/src/parking/Main.kt
+++ ParkingLot/Parking Lot/task/src/parking/Main.kt
@@ -1,7 +1,22 @@
 package parking
 
+var free = booleanArrayOf(false, true)
+
 fun main() {
-    println("White car has parked.")
-    println("Yellow car left the parking lot.")
-    println("Green car just parked here.")
+    val inputs = readLine()!!.split(" ")
+    val command = inputs[0]
+
+    if (command == "park") {
+        free[1] = false
+        println("${inputs[2]} car parked in spot 2.")
+    } else {
+        val leaveSpotNum = inputs[1].toInt()
+
+        if (free[leaveSpotNum - 1]) {
+            println("There is no car in spot $leaveSpotNum.")
+        } else {
+            free[1] = true
+            println("Spot $leaveSpotNum is free.")
+        }
+    }
 }
