cc9367054a054681e3f6e7b8775368f477a472fd
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-14 14:57:12 +0800

    第四阶段 Buy, fill, take 完成

--- CoffeeMachine/Coffee Machine/task/src/machine/Main.kt
+++ CoffeeMachine/Coffee Machine/task/src/machine/Main.kt
@@ -2,28 +2,78 @@ package machine
 
 import java.util.*
 
+val scanner = Scanner(System.`in`)
+
+val espresso = Coffee(250, beans = 16, money = 4)
+val latte = Coffee(350, 75, 20, 7)
+val cappuccino = Coffee(200, 100, 12, 6)
+
+var water = 400
+var milk = 540
+var beans = 120
+var cups = 9
+var money = 550
+
 fun main() {
-    val scanner = Scanner(System.`in`)
+    state()
 
-    println("Write how many ml of water the coffee machine has:")
-    val water = scanner.nextInt()
+    print("Write action (buy, fill, take):")
+    when (scanner.next()) {
+        "buy" -> buy()
+        "fill" -> fill()
+        "take" -> take()
+    }
 
-    println("Write how many ml of milk the coffee machine has:")
-    val milk = scanner.nextInt()
+    state()
+}
 
-    println("Write how many grams of coffee beans the coffee machine has:")
-    val gram = scanner.nextInt()
-
-    println("Write how many cups of coffee you will need:")
-    val tar = scanner.nextInt()
-
-    val cups = arrayOf(water / 200, milk / 50, gram / 15).minOf { it }
-
+fun state() {
     println(
-        when {
-            cups < tar -> "No, I can make only $cups cups of coffee"
-            cups > tar -> "Yes, I can make that amount of coffee (and even ${cups - tar} more than that)"
-            else -> "Yes, I can make that amount of coffee"
-        }
+        """
+        The coffee machine has:
+        $water of water
+        $milk of milk
+        $beans of coffee beans
+        $cups of disposable cups
+        $money of money
+    """.trimIndent()
     )
 }
+
+fun buy() {
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    when (scanner.nextInt()) {
+        1 -> updateState(espresso)
+        2 -> updateState(latte)
+        3 -> updateState(cappuccino)
+    }
+}
+
+fun updateState(coffee: Coffee) {
+    water -= coffee.water
+    milk -= coffee.milk
+    beans -= coffee.beans
+    money += coffee.money
+    cups--
+}
+
+fun fill() {
+    print("Write how many ml of water do you want to add:")
+    water += scanner.nextInt()
+
+    print("Write how many ml of milk do you want to add:")
+    milk += scanner.nextInt()
+
+    print("Write how many grams of coffee beans do you want to add:")
+    beans += scanner.nextInt()
+
+    print("Write how many disposable cups of coffee do you want to add:")
+    cups += scanner.nextInt()
+}
+
+fun take() {
+    println("I gave you $$money")
+    money = 0
+}
+
+data class Coffee(var water: Int = 0, var milk: Int = 0, var beans: Int = 0, var money: Int = 0)
