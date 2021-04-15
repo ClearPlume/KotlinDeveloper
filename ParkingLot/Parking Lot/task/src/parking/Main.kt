db913f4574c97020819a38b185ca4c1bde88d4ba
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-15 16:14:38 +0800

    第四阶段 Size matters 完成

--- ParkingLot/Parking Lot/task/src/parking/Main.kt
+++ ParkingLot/Parking Lot/task/src/parking/Main.kt
@@ -1,32 +1,73 @@
 package parking
 
-var state = Array(20) { false }
+var state: MutableMap<Int, String>? = null
 
 fun main() {
     while (true) {
         val input = readLine()!!
         if (input == "exit") break
         val inputs = input.split(' ')
-        val command = inputs[0]
 
-        if (command == "park") {
-            if (state.none { !it }) {
-                println("Sorry, the parking lot is full.")
-                continue
+        when (inputs[0]) {
+            "create" -> {
+                val spotsNumber = inputs[1].toInt()
+
+                if (isParkingLotCreated()) {
+                    state!!.clear()
+                } else {
+                    state = HashMap()
+                }
+
+                for (num in 1..spotsNumber) {
+                    state!![num] = ""
+                }
+
+                println("Created a parking lot with $spotsNumber spots.")
             }
-            val carStopIndex = state.indexOfFirst { !it }
-            state[carStopIndex] = true
-            println("${inputs[2]} car parked in spot ${carStopIndex + 1}.")
-        } else {
-            val leaveSpotNum = inputs[1].toInt()
-            val leaveSpotIndex = leaveSpotNum - 1
+            "park" -> {
+                if (isParkingLotCreated()) {
+                    if (state!!.none { it.value.isEmpty() }) {
+                        println("Sorry, the parking lot is full.")
+                        continue
+                    }
+                    val carNumber = inputs[1]
+                    val carColor = inputs[2]
+                    val carStopPosition = state!!.entries.first { it.value.isEmpty() }.key
+                    state!![carStopPosition] = "$carNumber $carColor"
+                    println("$carColor car parked in spot $carStopPosition.")
+                } else {
+                    println("Sorry, a parking lot has not been created.")
+                }
+            }
+            "leave" -> {
+                if (isParkingLotCreated()) {
+                    val leaveSpotNum = inputs[1].toInt()
 
-            if (!state[leaveSpotIndex]) {
-                println("There is no car in spot $leaveSpotNum.")
-            } else {
-                state[leaveSpotIndex] = false
-                println("Spot $leaveSpotNum is free.")
+                    if (state!![leaveSpotNum]?.isEmpty() == true) {
+                        println("There is no car in spot $leaveSpotNum.")
+                    } else {
+                        state!![leaveSpotNum] = ""
+                        println("Spot $leaveSpotNum is free.")
+                    }
+                } else {
+                    println("Sorry, a parking lot has not been created.")
+                }
+            }
+            "status" -> {
+                if (isParkingLotCreated()) {
+                    if (state!!.filterValues { it.isNotEmpty() }.isEmpty()) {
+                        println("Parking lot is empty.")
+                    } else {
+                        for (parking in state!!.filterValues { it.isNotEmpty() }) {
+                            println("${parking.key} ${parking.value}")
+                        }
+                    }
+                } else {
+                    println("Sorry, a parking lot has not been created.")
+                }
             }
         }
     }
 }
+
+fun isParkingLotCreated() = state != null
