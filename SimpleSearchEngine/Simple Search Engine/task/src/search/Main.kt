e12171035fee2decccb65eba4b69c5d121686bbf
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-16 17:11:34 +0800

    第一阶段 String theory 完成

--- SimpleSearchEngine/Simple Search Engine/task/src/search/Main.kt
+++ SimpleSearchEngine/Simple Search Engine/task/src/search/Main.kt
@@ -1,5 +1,13 @@
 package search
 
+import java.util.*
+
 fun main() {
-    println("Hello, World!")
+    val scanner = Scanner(System.`in`)
+
+    val words = scanner.nextLine().split(' ')
+    val keywords = scanner.next()
+
+    val index = words.indexOf(keywords)
+    println(if (index != -1) index + 1 else "Not found")
 }
