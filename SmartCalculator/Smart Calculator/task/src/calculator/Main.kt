a3dc588466a695b0fb828ba621748df7584fe06b
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-21 09:53:47 +0800

    第三阶段 Count them all 完成

--- SmartCalculator/Smart Calculator/task/src/calculator/Main.kt
+++ SmartCalculator/Smart Calculator/task/src/calculator/Main.kt
@@ -7,6 +7,7 @@ fun main() {
         when (val input = Scanner(System.`in`).nextLine().trim()) {
             "/exit" -> break
             "" -> continue
+            "/help" -> println("The program calculates the sum of numbers")
             else -> println(input.split(" ").sumBy { it.toInt() })
         }
     }
