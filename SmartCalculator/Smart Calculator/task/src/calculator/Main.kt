d4020f9604028011c251048b57a60e3772712fac
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-21 09:51:38 +0800

    第二阶段 2 + 2 + 完成

--- SmartCalculator/Smart Calculator/task/src/calculator/Main.kt
+++ SmartCalculator/Smart Calculator/task/src/calculator/Main.kt
@@ -1,17 +1,14 @@
 package calculator
 
 import java.util.*
-import kotlin.system.exitProcess
 
 fun main() {
     while (true) {
-        when (val input = Scanner(System.`in`).nextLine()) {
-            "/exit" -> {
-                println("Bye!")
-                exitProcess(0)
-            }
+        when (val input = Scanner(System.`in`).nextLine().trim()) {
+            "/exit" -> break
             "" -> continue
             else -> println(input.split(" ").sumBy { it.toInt() })
         }
     }
+    println("Bye!")
 }
