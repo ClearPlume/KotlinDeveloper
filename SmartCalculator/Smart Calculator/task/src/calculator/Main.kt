96036885b146701e79738bd1cdb03c8dc67390c9
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-21 12:41:39 +0800

    第五阶段 Error! 完成

--- SmartCalculator/Smart Calculator/task/src/calculator/Main.kt
+++ SmartCalculator/Smart Calculator/task/src/calculator/Main.kt
@@ -4,42 +4,53 @@ import java.util.*
 
 fun main() {
     while (true) {
-        when (val input = Scanner(System.`in`).nextLine().trim()) {
-            "/exit" -> break
-            "" -> continue
-            "/help" -> println(
-                """
+        val input = Scanner(System.`in`).nextLine().trim()
+
+        if (input.startsWith("/") || input == "") {
+            when (input) {
+                "" -> continue
+                "/exit" -> break
+                "/help" -> println(
+                    """
                     The program calculates the sum of numbers,
                     handling addition and (binary and unary) subtraction,
                     ignoring whitespace.
                 """.trimIndent()
-            )
-            else -> {
-                val operators = input.split(Regex("\\s+"))
-                var operation = Operation.NONE
-
-                val result = operators.fold(operators[0].toInt()) { acc, ele ->
-                    if (ele.matches(Regex("-?\\d+"))) {
-                        if (operation == Operation.NONE) {
-                            return@fold acc
-                        } else {
-                            val cur = when (operation) {
-                                Operation.ADDITION -> acc + ele.toInt()
-                                Operation.SUBTRACTION -> acc - ele.toInt()
-                                else -> acc
-                            }
-
-                            operation = Operation.NONE
-                            return@fold cur
-                        }
-                    } else {
-                        operation = Operation.checkOperation(ele)
-                        return@fold acc
-                    }
+                )
+                else -> {
+                    println("Unknown command")
+                    continue
                 }
-
-                println(result)
             }
+        }
+
+        if (input.matches(Regex("[-+]*\\d+(?: +[+-]+ +[-+]*\\d+)*"))) {
+            val operators = input.split(Regex("\\s+"))
+            var operation = Operation.NONE
+
+            val result = operators.fold(operators[0].toInt()) { acc, ele ->
+                if (ele.matches(Regex("[-+]*\\d+"))) {
+                    if (operation == Operation.NONE) {
+                        return@fold acc
+                    } else {
+                        val cur = when (operation) {
+                            Operation.ADDITION -> acc + ele.toInt()
+                            Operation.SUBTRACTION -> acc - ele.toInt()
+                            else -> acc
+                        }
+
+                        operation = Operation.NONE
+                        return@fold cur
+                    }
+                } else {
+                    operation = Operation.checkOperation(ele)
+                    return@fold acc
+                }
+            }
+
+            println(result)
+        } else {
+            println("Invalid expression")
         }
     }
     println("Bye!")
