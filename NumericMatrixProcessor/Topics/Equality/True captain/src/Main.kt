7723943d6a56e66f5f98133c59d16f2c4616f824
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-23 15:15:20 +0800

    每日问题解决

--- NumericMatrixProcessor/Topics/Equality/True captain/src/Main.kt
+++ NumericMatrixProcessor/Topics/Equality/True captain/src/Main.kt
@@ -1,3 +1,7 @@
-val captain = Pirate("Hector Barbossa")
+var captain = Pirate("Hector Barbossa")
 println(captain.name) // Do not touch this line
 // put your code here
+captain = Pirate("Jack Sparrow")
+println(captain.name)
+
+data class Pirate(val name: String)
