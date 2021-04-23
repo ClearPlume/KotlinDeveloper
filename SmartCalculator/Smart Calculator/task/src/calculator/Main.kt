9db0086f56d2d98f68280068d57a23fd9f7bbabd
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-23 10:54:44 +0800

    第七阶段 I’ve got the power 完成

--- SmartCalculator/Smart Calculator/task/src/calculator/Main.kt
+++ SmartCalculator/Smart Calculator/task/src/calculator/Main.kt
@@ -1,10 +1,15 @@
 package calculator
 
-val variableRegex = Regex("[a-zA-Z]+")
-val assignmentRegex = Regex("[a-zA-Z]+ *= *(?:[a-zA-Z]+|\\d+)")
-val expressionRegex = Regex("(?:(?:[-+]*\\d+)|[a-zA-Z]+)(?: +[+-]+ +(?:(?:[-+]*\\d+)|[a-zA-Z]+))*")
+import java.util.*
+import kotlin.math.pow
 
-val variables = mutableMapOf<String, Int>()
+private val variableRegex = Regex("[a-zA-Z]+")
+private val assignmentRegex = Regex("[a-zA-Z]+ *= *(?:[a-zA-Z]+|[+-]*\\d+)")
+private val expressionRegex =
+    Regex("\\(*(?:(?:[-+]*\\d+)|[a-zA-Z]+)\\)*(?: *\\(*(?:[+-]+|[*/^])\\)* *\\(*(?:(?:[-+]*\\d+)|[a-zA-Z]+)\\)*)*")
+val operatorRegex = Regex("[+-]+|[*/^()]")
+
+private val variables = mutableMapOf<String, Int>()
 
 fun main() {
     while (true) {
@@ -19,15 +24,19 @@ fun main() {
                     "/exit" -> break
                     "/help" -> println(
                         """
-                        This is a calculator. It supports addition, subtraction and saving variables
+                        This is a calculator. It supports addition, subtraction, multiplication, division and saving variables
                         |Examples:
-                        |> 1 -- 2 --- 3 ++++ -10 = -10
+                        |> 1 -- 2 --- 3 ++++ -10 * 2 + 1 = -19
                         |> a = 2
                         |> b = a
                         |> b
                         | 2
                         |> c
                         | Unknown variable
+                        |> a *** b
+                        | Invalid expression
+                        |> a // b)
+                        | Invalid expression
                         | WARNING: only letters in variables names
                         |type /help to see this text
                         |type /exit to end the program
@@ -69,31 +78,12 @@ fun main() {
             }
             // expression
             else -> {
-                if (input.matches(expressionRegex)) {
-                    val operators = input.split(Regex("\\s+"))
-                    var operation = Operation.NONE
-
-                    val result = operators.fold(0) { acc, ele ->
-                        if (ele.matches(Regex("[-+]*\\d+")) || ele.matches(variableRegex)) {
-                            if (operation == Operation.NONE) {
-                                return@fold acc + ele.getVarValue()
-                            } else {
-                                val cur = when (operation) {
-                                    Operation.ADDITION -> acc + ele.getVarValue()
-                                    Operation.SUBTRACTION -> acc - ele.getVarValue()
-                                    else -> acc
-                                }
-
-                                operation = Operation.NONE
-                                return@fold cur
-                            }
-                        } else {
-                            operation = Operation.checkOperation(ele)
-                            return@fold acc
-                        }
-                    }
-
-                    println(result)
+                if (input.matches(expressionRegex, true)) {
+                    // Parse [expression] into List<[ExpressionUnit]>
+                    val infixExpression = parseExpression(StringBuilder(input.replace(Regex("\\s+"), "")))
+                    // From infix to postfix
+                    val postfixExpression = infixToPost(infixExpression)
+                    println(postfixCalculate(postfixExpression))
                 } else {
                     println("Invalid expression")
                 }
@@ -103,48 +93,300 @@ fun main() {
     println("Bye!")
 }
 
-private fun String.getVarValue(): Int {
-    return if (this.isNumeric()) {
-        this.toInt()
-    } else {
-        if (this in variables) {
-            variables[this]!!
-        } else {
-            0
-        }
-    }
-}
-
+/**
+ * For assignment statement, check whether the user's input is a number or a variable
+ */
 private fun String.isNumeric(): Boolean {
     for (c in this) {
-        if (c.notDigit()) return false
+        if (!c.isNum()) return false
     }
     return true
 }
 
-private fun Char.notDigit(): Boolean {
-    if (this != '+' && this != '-' && this in '0'..'9') return false
-    return true
+/**
+ * For assignment statement's check, because there may be these "numbers": -10 10 +10 --10 ++10 -+-10 etc
+ */
+private fun Char.isNum(): Boolean {
+    if (this in arrayOf('+', '-') || this in '0'..'9') return true
+    return false
 }
 
+/**
+ * For assignment statement, cast from numeric string to a number, because there may be these "numbers": -10 10 +10 --10 ++10 -+-10 etc
+ */
 private fun String.toInt(): Int {
     val splitPoint = indexOfLast { it == '+' || it == '-' }
-    val modifier = if (splitPoint != -1) Operation.checkOperation(substring(0 until splitPoint)) else null
+    val modifier = if (splitPoint != -1) Operator.checkPlusMinus(substring(0 until splitPoint)) else null
     val number = Integer.parseInt(substring(if (splitPoint == -1) 0 else splitPoint))
 
-    return if (modifier == Operation.SUBTRACTION) number * -1 else number
+    return if (modifier == Operator.SUBTRACTION) number * -1 else number
 }
 
-enum class Operation {
-    NONE, ADDITION, SUBTRACTION;
+/**
+ * For expressions, check whether the user's input has paired brackets
+ */
+private fun String.matches(regex: Regex, checkBrackets: Boolean): Boolean {
+    val brackets = Stack<Char>()
 
-    companion object {
-        fun checkOperation(operationSequence: String): Operation {
-            return operationSequence.fold(ADDITION) { acc, operator -> if (operator == '-') !acc else acc }
+    if (checkBrackets) {
+        for (char in this) {
+            if (char == '(') brackets.push(char)
+
+            if (char == ')') {
+                if (brackets.empty()) {
+                    brackets.push(char)
+                    break
+                } else {
+                    brackets.pop()
+                }
+            }
         }
     }
 
-    private operator fun not(): Operation {
-        return if (this == ADDITION) SUBTRACTION else ADDITION
+    return regex.matches(this) && brackets.empty()
+}
+
+/**
+ * Parse [expression] into List<[ExpressionUnit]>
+ */
+private fun parseExpression(expression: StringBuilder): List<ExpressionUnit> {
+    val expressions = mutableListOf<ExpressionUnit>()
+
+    run {
+        var unit: ExpressionUnit?
+
+        do {
+            unit = expression.nextUnit()
+
+            if (unit != null) {
+                expressions.add(unit)
+            }
+        } while (unit != null)
     }
+
+    lastUnitIndex = 0
+    return expressions
+}
+
+private var lastUnitIndex: Int = 0
+
+private fun StringBuilder.nextUnit(): ExpressionUnit? {
+    var next = StringBuilder()
+    var lastChar: Char
+    var lastNumber = false
+
+    if (lastUnitIndex < lastIndex) {
+        lastChar = get(lastUnitIndex)
+        next = next.append(lastChar)
+
+        for (i in lastUnitIndex + 1..lastIndex) {
+            val c = get(i)
+
+            if (lastChar same c) {
+                lastChar = c
+                next = next.append(lastChar)
+                lastUnitIndex = i
+            } else {
+                lastUnitIndex = i
+                break
+            }
+
+            lastNumber = i == lastIndex
+        }
+    } else if (lastUnitIndex == lastIndex) {
+        next = next.append(last())
+        lastUnitIndex++
+    }
+
+    val nextStr = next.toString()
+
+    if (lastNumber) lastUnitIndex = length
+
+    return when {
+        nextStr.matches(variableRegex) -> Variable(nextStr)
+        nextStr.isNumber() -> Number(nextStr.toInt())
+        next.matches(operatorRegex) -> Operator.get(nextStr)
+        else -> null
+    }
+}
+
+private infix fun Char.same(other: Char): Boolean {
+    return (toString() + other).matches(Regex("\\d+|[+-]+"))
+}
+
+/**
+ * From infix to postfix
+ */
+private fun infixToPost(expression: List<ExpressionUnit>): List<ExpressionUnit> {
+    val result = Stack<ExpressionUnit>()
+    val operator = Stack<ExpressionUnit>()
+
+    for (unit in expression) {
+        when (unit) {
+            is Number -> result.push(unit)
+            is Variable -> result.push(unit.toNumber())
+            is Operator -> {
+                if (unit.isBracket()) {
+                    if (unit.literal == '(') {
+                        operator.push(unit)
+                    } else {
+                        var operate = operator.pop()
+
+                        while (operate.literal != '(') {
+                            result.push(operate)
+                            if (operator.empty()) {
+                                break
+                            } else {
+                                operate = operator.pop()
+                            }
+                        }
+                    }
+                } else {
+                    if (operator.empty() || operator.peek().literal == '(') {
+                        operator.push(unit)
+                    } else {
+                        if (unit > operator.peek()) {
+                            operator.push(unit)
+                        } else {
+                            while (operator.isNotEmpty() && unit <= operator.peek()) {
+                                result.push(operator.pop())
+                                if (operator.isNotEmpty() && operator.peek().literal == '(') break
+                            }
+                            operator.push(unit)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    while (operator.isNotEmpty()) {
+        result.push(operator.pop())
+    }
+
+    return result.toList()
+}
+
+private fun postfixCalculate(expression: List<ExpressionUnit>): Int {
+    val calculate = Stack<ExpressionUnit>()
+
+    for (unit in expression) {
+        when (unit) {
+            is Number -> calculate.push(unit)
+            is Operator -> calculate.push(unit.calc(calculate.pop(), calculate.pop()))
+        }
+    }
+
+    return calculate.pop().literal as Int
+}
+
+/**
+ * For assignment statement, check whether the user's input is a number or a variable
+ */
+private fun String.isNumber(): Boolean {
+    if (this == "") return false
+
+    for (c in this) {
+        if (!c.isDigit()) return false
+    }
+
+    return true
+}
+
+/**
+ * This is a base unit in a expression, could be [Number], [Operator], [Variable]
+ * I use a list of [ExpressionUnit] to represents an expression
+ */
+private open class ExpressionUnit(val priority: Int, open val literal: Any)
+
+/**
+ * 10, 20, 30...
+ */
+private class Number(override val literal: Int) : ExpressionUnit(0, literal) {
+    operator fun plus(other: Number) = literal + other.literal
+
+    operator fun minus(other: Number) = literal - other.literal
+
+    operator fun times(other: Number) = literal * other.literal
+
+    operator fun div(other: Number) = literal / other.literal
+
+    infix fun pow(other: Number) = literal.pow(other.literal)
+}
+
+private fun Int.pow(other: Int): Int {
+    val a = this.toDouble()
+    val b = other.toDouble()
+
+    return a.pow(b).toInt()
+}
+
+/**
+ * a, b, c, test, age...
+ */
+private class Variable(override val literal: String) : ExpressionUnit(0, literal) {
+    fun toNumber() = Number(variables[literal]!!)
+}
+
+/**
+ * +, -, *, /, ^, (, )...
+ */
+private class Operator private constructor(priority: Int, override val literal: Char) :
+    ExpressionUnit(priority, literal) {
+    companion object {
+        // For unexpected situation
+        val NONE = Operator(-1, '\u0000')
+        val ADDITION = Operator(1, '+')
+        val SUBTRACTION = Operator(1, '-')
+        val MULTIPLICATION = Operator(2, '*')
+        val DIVISION = Operator(2, '/')
+        val EXPONENTIATION = Operator(3, '^')
+        val LEFT_BRACKET = Operator(4, '(')
+        val RIGHT_BRACKET = Operator(4, ')')
+
+        /**
+         * Convert "+++", "---", "+-+" to '+' or '-'
+         */
+        fun checkPlusMinus(operatorSequence: String): Operator {
+            return if (operatorSequence.count { it == '-' } % 2 == 0) {
+                ADDITION
+            } else {
+                SUBTRACTION
+            }
+        }
+
+        fun get(operator: String): Operator {
+            return when (if (operator.length > 1) checkPlusMinus(operator).literal else operator[0]) {
+                '+' -> ADDITION
+                '-' -> SUBTRACTION
+                '*' -> MULTIPLICATION
+                '/' -> DIVISION
+                '^' -> EXPONENTIATION
+                '(' -> LEFT_BRACKET
+                ')' -> RIGHT_BRACKET
+                else -> NONE
+            }
+        }
+    }
+
+    fun isBracket() = literal in arrayOf('(', ')')
+
+    fun calc(first: ExpressionUnit, second: ExpressionUnit): Number {
+        first as Number
+        second as Number
+        val result = when (literal) {
+            '+' -> second + first
+            '-' -> second - first
+            '*' -> second * first
+            '/' -> second / first
+            else -> second pow first
+        }
+
+        return Number(result)
+    }
+
+    /**
+     * Compare this operator's priority
+     */
+    operator fun compareTo(other: ExpressionUnit) = priority - other.priority
 }
---
+++ SmartCalculator/Topics/Multi-dimensional array/Cube of zeros/src/Main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val cube = Array(3) { Array(3) { intArrayOf(0, 0, 0) } }
+    println(cube.contentDeepToString())
+}
