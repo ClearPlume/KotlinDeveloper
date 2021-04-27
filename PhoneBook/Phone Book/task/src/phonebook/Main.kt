1330e7408d9572dfb825593301478f490801bb03
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-27 10:05:50 +0800

    第一阶段 A needle in the hay 完成

--- PhoneBook/Phone Book/task/src/phonebook/Main.kt
+++ PhoneBook/Phone Book/task/src/phonebook/Main.kt
@@ -1,5 +1,36 @@
 package phonebook
 
+import java.io.File
+import java.util.*
+
 fun main() {
-    println("Hello, World!")
+    val fileDirectory = File("E:\\directory.txt")
+    val directory = mutableListOf<String>()
+    fileDirectory.forEachLine { directory.add(it) }
+
+    val fileFind = File("E:\\find.txt")
+    val find = mutableListOf<String>()
+    fileFind.forEachLine { find.add(it) }
+    val allFindEntryNum = find.size
+    var foundEntryNum = 0
+
+    val timeMillisBeforeSearch = System.currentTimeMillis()
+    println("Start searching...")
+
+    for (item in find) {
+        if (directory.find { item in it } != null) {
+            foundEntryNum++
+        }
+    }
+
+    val timeTaken = Calendar.getInstance()
+    timeTaken.timeInMillis = System.currentTimeMillis() - timeMillisBeforeSearch
+
+    println(
+        "Found $allFindEntryNum / $foundEntryNum entries. Time taken: ${timeTaken.get(Calendar.MINUTE)} min. ${
+            timeTaken.get(
+                Calendar.SECOND
+            )
+        } sec. ${timeTaken.get(Calendar.MILLISECOND)}ms."
+    )
 }
