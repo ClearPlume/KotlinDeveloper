c3fb05b3f4e618924908053ed323a6fdb5badbcd
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-29 17:18:48 +0800

    第一阶段 Numbers only 完成

--- SortingTool/Sorting Tool/task/src/sorting/Main.kt
+++ SortingTool/Sorting Tool/task/src/sorting/Main.kt
@@ -1,5 +1,20 @@
 package sorting
 
+import java.util.*
+
 fun main() {
     // write your code here
+    val scanner = Scanner(System.`in`)
+    val numbers = mutableListOf<Int>()
+
+    while (scanner.hasNextInt()) {
+        numbers.add(scanner.nextInt())
+    }
+
+    val total = numbers.size
+    val max = numbers.maxOrNull()!!
+    val maxNum = numbers.count { it == max }
+
+    println("Total numbers: $total.")
+    println("The greatest number: $max ($maxNum time(s)).")
 }
