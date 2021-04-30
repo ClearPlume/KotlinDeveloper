5c133d9369ed680a7f588899ba6461478efde326
杜海蛟 <the.fallenangel.965@gmail.com>
2021-04-30 18:38:20 +0800

    第五阶段 Error! 完成

--- SortingTool/Sorting Tool/task/src/sorting/Main.kt
+++ SortingTool/Sorting Tool/task/src/sorting/Main.kt
@@ -1,28 +1,61 @@
 package sorting
 
 import java.util.*
+import kotlin.math.roundToInt
+import kotlin.system.exitProcess
+
+val commandList = arrayOf("sortingType", "dataType")
 
 fun main(args: Array<String>) {
     // write your code here
-    val sortingTool = SortingTool(args)
+    val argMap = args.parse()
+    val sortingTool = SortingTool(argMap)
     sortingTool.info()
 }
 
-fun Array<String>.get(name: String, missingArgValue: String): String {
-    return (indexOf("-$name")).let { if (it != -1) this[it + 1] else missingArgValue }
-}
+fun Array<String>.parse(): Map<String, String> {
+    val args = mutableMapOf<String, String>()
 
-fun MutableList<Pair<SortingElement, Int>>.find(element: SortingElement): Pair<SortingElement, Int>? {
-    var pair: Pair<SortingElement, Int>? = null
-
-    for (item in this) {
-        if (item.first == element) {
-            pair = item
-            break
+    for (i in indices) {
+        if (this[i].startsWith('-')) {
+            if (this[i].substring(1) in commandList) {
+                if (i < lastIndex && !this[i + 1].startsWith('-')) {
+                    args[this[i].substring(1)] = this[i + 1]
+                }
+            } else {
+                println("\"${this[i]}\" is not a valid parameter. It will be skipped.")
+            }
         }
     }
 
-    return pair
+    return args
+}
+
+fun Array<String>.get(name: String): String? {
+    var arg: String? = null
+
+    for (i in this.indices) {
+        if (this[i].startsWith('-')) {
+            if ("-$name" == this[i]) {
+                if (!this[i + 1].startsWith('-')) {
+                    arg = this[i + 1]
+                    break
+                }
+            }
+        }
+    }
+
+    return arg
+}
+
+fun String.isNumeric(): Boolean {
+    for (c in this) {
+        if (!c.isDigit() && c != '-') {
+            return false
+        }
+    }
+
+    return true
 }
 
 operator fun MutableList<Pair<SortingElement, Int>>.get(element: SortingElement): Int {
@@ -40,33 +73,58 @@ operator fun MutableList<Pair<SortingElement, Int>>.set(element: SortingElement,
     }
 }
 
-class SortingTool(args: Array<String>) {
+fun MutableList<Pair<SortingElement, Int>>.find(element: SortingElement): Pair<SortingElement, Int>? {
+    var pair: Pair<SortingElement, Int>? = null
+
+    for (item in this) {
+        if (item.first == element) {
+            pair = item
+            break
+        }
+    }
+
+    return pair
+}
+
+class SortingTool(args: Map<String, String>) {
     private val scanner = Scanner(System.`in`)
-    private val type = args.get("dataType", "word")
-    private val sort = args.get("sortingType", "natural")
+    private val type = args["dataType"] ?: "word"
+    private val sort = args["sortingType"] ?: "natural"
     private val data = mutableListOf<SortingElement>()
     private val dataSorted = mutableListOf<Pair<SortingElement, Int>>()
 
     private val total: Int
 
     init {
-        when (type) {
-            "long" -> {
-                while (scanner.hasNextLong()) {
-                    data.add(Long(scanner.nextLong()))
+        if (type !in arrayOf("long", "word", "line")) {
+            println("No data type defined!")
+            exitProcess(0)
+        }
+
+        if (sort !in arrayOf("natural", "byCount")) {
+            println("No sorting type defined!")
+            exitProcess(0)
+        }
+
+        if (type != "line") {
+            // \\S: All non blank characters
+            // +: At least one character
+            while (scanner.hasNext("\\S+")) {
+                val next = scanner.next("\\S+")
+                when (type) {
+                    "long" -> {
+                        if (next.isNumeric()) {
+                            data.add(Long(next.toLong()))
+                        } else {
+                            println("\"$next\" is not a long. It will be skipped.")
+                        }
+                    }
+                    "word" -> data.add(Word(next))
                 }
             }
-            "line" -> {
-                while (scanner.hasNextLine()) {
-                    data.add(Line(scanner.nextLine()))
-                }
-            }
-            "word" -> {
-                // \\S: All non blank characters
-                // +: At least one character
-                while (scanner.hasNext("\\S+")) {
-                    data.add(Word(scanner.next("\\S+")))
-                }
+        } else {
+            while (scanner.hasNextLine()) {
+                data.add(Line(scanner.nextLine()))
             }
         }
 
@@ -88,7 +146,7 @@ class SortingTool(args: Array<String>) {
         when (sort) {
             "natural" -> println("Sorted data: ${data.joinToString(if (type == "line") "\n" else " ")}")
             "byCount" -> for (item in dataSorted) {
-                println("${item.first}: ${item.second} time(s), ${((item.second.toDouble() / total) * 100).toInt()}%")
+                println("${item.first}: ${item.second} time(s), ${((item.second.toDouble() / total) * 100).roundToInt()}%")
             }
         }
     }
