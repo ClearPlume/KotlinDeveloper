9b0f263d765e53fea55f023b88242f7c1dbbfdf3
杜海蛟 <the.fallenangel.965@gmail.com>
2021-05-03 17:10:47 +0800

    第三阶段 Make it your own 完成

--- Flashcards/Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/Flashcards/task/src/flashcards/Main.kt
@@ -1,6 +1,28 @@
 package flashcards
 
+import java.util.*
+
 fun main() {
-    val (_, definition, answer) = Array(3) { readLine()!! }
-    println(if (definition == answer) "Your answer is right!" else "Your answer is wrong...")
+    val scanner = Scanner(System.`in`)
+
+    println("Input the number of cards:")
+    val cardNum = scanner.nextLine().toInt()
+    val cards = mutableMapOf<String, String>()
+
+    for (num in 1..cardNum) {
+        println("Card #$num:")
+        val term = scanner.nextLine()
+
+        println("The definition for card #$num:")
+        val definition = scanner.nextLine()
+
+        cards[term] = definition
+    }
+
+    for (card in cards) {
+        println("Print the definition of \"${card.key}\":")
+        val userInput = scanner.nextLine()
+
+        println(if (userInput == card.value) "Correct!" else "Wrong. The right answer is \"${card.value}\".")
+    }
 }
