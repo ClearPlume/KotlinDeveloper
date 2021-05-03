53a7cd58d245e195d34b0bd64df8965a4a0f392b
杜海蛟 <the.fallenangel.965@gmail.com>
2021-05-03 18:35:12 +0800

    第四阶段 A good stack 完成

--- Flashcards/Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/Flashcards/task/src/flashcards/Main.kt
@@ -2,19 +2,19 @@ package flashcards
 
 import java.util.*
 
-fun main() {
-    val scanner = Scanner(System.`in`)
+val scanner = Scanner(System.`in`)
+val cards = mutableMapOf<String, String>()
 
+fun main() {
     println("Input the number of cards:")
     val cardNum = scanner.nextLine().toInt()
-    val cards = mutableMapOf<String, String>()
 
     for (num in 1..cardNum) {
         println("Card #$num:")
-        val term = scanner.nextLine()
+        val term = getInput(InputType.TERM)
 
         println("The definition for card #$num:")
-        val definition = scanner.nextLine()
+        val definition = getInput(InputType.DEFINITION)
 
         cards[term] = definition
     }
@@ -23,6 +23,60 @@ fun main() {
         println("Print the definition of \"${card.key}\":")
         val userInput = scanner.nextLine()
 
-        println(if (userInput == card.value) "Correct!" else "Wrong. The right answer is \"${card.value}\".")
+        if (userInput == card.value) {
+            println("Correct!")
+        } else {
+            val answer = cards[card.key]
+            val another = cards.findKbyV(userInput)
+            val rightAnother = userInput in cards.values
+
+            println("Wrong. The right answer is \"$answer\"${if (rightAnother) ", but your definition is correct for \"$another\"." else "."}")
+        }
     }
 }
+
+fun getInput(type: InputType): String {
+    var input: String
+
+    when (type) {
+        InputType.TERM -> {
+            while (true) {
+                input = scanner.nextLine()
+
+                if (input in cards) {
+                    println("The term \"$input\" already exists. Try again:")
+                } else {
+                    break
+                }
+            }
+        }
+        InputType.DEFINITION -> {
+            while (true) {
+                input = scanner.nextLine()
+
+                if (input in cards.values) {
+                    println("The definition \"$input\" already exists. Try again:")
+                } else {
+                    break
+                }
+            }
+        }
+    }
+
+    return input
+}
+
+fun <K, V> MutableMap<K, V>.findKbyV(value: V): K? {
+    var k: K? = null
+
+    for (entry in this) {
+        if (entry.value == value) {
+            k = entry.key
+            break
+        }
+    }
+
+    return k
+}
+
+enum class InputType { TERM, DEFINITION }
