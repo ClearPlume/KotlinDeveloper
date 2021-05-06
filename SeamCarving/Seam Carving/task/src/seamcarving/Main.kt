4208e6168a9da1b493084baa11dfed0c002de7fc
杜海蛟 <the.fallenangel.965@gmail.com>
2021-05-06 18:49:34 +0800

    第一阶段 Create an image 完成

--- SeamCarving/Seam Carving/task/src/seamcarving/Main.kt
+++ SeamCarving/Seam Carving/task/src/seamcarving/Main.kt
@@ -1,5 +1,29 @@
 package seamcarving
 
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import java.util.*
+import javax.imageio.ImageIO
+
 fun main() {
-    print("Hello, World!")
+    val scanner = Scanner(System.`in`)
+
+    println("Enter rectangle width:")
+    val width = scanner.nextInt()
+
+    println("Enter rectangle height:")
+    val height = scanner.nextInt()
+
+    val image = BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR)
+
+    println("Enter output image name:")
+    val imgName = scanner.next()
+
+    val graphics = image.createGraphics()
+    graphics.color = Color.RED
+    graphics.drawLine(0, 0, width - 1, height - 1)
+    graphics.drawLine(width - 1, 0, 0, height - 1)
+
+    ImageIO.write(image, "png", File(imgName))
 }
